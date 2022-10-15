3293b26994f6d38a9e40670e9f30974c05bf4978
Tourki <hos@dr.com>
2022-10-15 19:50:19 +0200

    Support first two new features
    added support for ignoring all whitespaces commands
    added support for direct command calls without full path

--- Assignment_3/shell.c
+++ Assignment_3/shell.c
@@ -1,22 +1,46 @@
+#define _GNU_SOURCE
 #include <stdio.h>
+#include <stdbool.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <string.h>
 
+
+bool AllIsWhiteSpaces(char *s)
+{
+	int  i=0;
+	for(i=0;s[i]!='\0';i++)
+    {
+		if(s[i]!=' ' && s[i]!='\t')
+			return false;
+    }
+	return true;
+}
+
 int main(int argc, char *argv[])
 {
-	while (1){
-		char buf[100];
-		fgets(buf,100,stdin);
-		int ret_pid = fork();
+	char buf[100]="";
+	char *cmdargv[] = { NULL };
+	int status;
 
-		if (ret_pid < 0)
-			printf("Fork failed\n");
-		else if (ret_pid == 0){
-			char *cmdargv[] = { NULL };
-			char *cmdenvp[] = { NULL };
-			buf[strlen(buf)-1]=0;
-			execve(buf,cmdargv,cmdenvp);
+	while (1){
+		printf("tourki@localshell$ ");
+		gets(buf);
+		if (AllIsWhiteSpaces(buf) == false){
+
+			int ret_pid = fork();
+
+			if (ret_pid < 0)
+				printf("Fork failed\n");
+			else if (ret_pid > 0){
+				wait(&status);
+			}
+			else if (ret_pid == 0){
+				execvp(buf,cmdargv);
+				printf("%s: command not found\n",buf);
+				break;
+			}
 		}
 	}
 
